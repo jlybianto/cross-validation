045f09b6a792c7814c7bca5f9e365c4c66d51b92
jlybianto <jesse.lybianto@gmail.com>
2016-03-22 23:14:28 -0600

    Input modeling of data

--- cv.py
+++ cv.py
@@ -22,3 +22,51 @@ data["Species"] = iris.target
 print(data)
 print("")
 
+# ----------------
+# MODEL DATA (WITHOUT SPLITTING INTO TRAINING / TESTING)
+# ----------------
+
+# Dataset not split into training and testing
+print("Data points in Data Set: " + str(len(data)))
+svc = svm.SVC(kernel="linear")
+x = iris.data
+y = iris.target
+svc.fit(x, y)
+
+predict = svc.predict(x)
+expect = y
+match = 0
+for i in predict:
+	if i == expect[i]:
+		match += 1
+
+score = round((float(match) / len(iris.target)) * 100, 1)
+print("Number of Matches: " + str(match))
+print("Score: " + str(score))
+print("")
+
+# ----------------
+# MODEL DATA (WITH SPLITTING INTO TRAINING / TESTING)
+# ----------------
+
+# Split dataset into training and testing (60-40 ratio)
+train, test = train_test_split(data, test_size=0.4)
+print("Data points in Training Set: " + str(len(train)))
+print("Data points in Test Set: " + str(len(test)))
+
+svc_split = svm.SVC(kernel="linear")
+x_split = train.ix[:, 0:4]
+y_split = train.ix[:, 4]
+svc_split.fit(x_split, y_split)
+
+predict_split = svc.predict(test.ix[:, 0:4])
+expect_split = np.array(test.ix[:, 4])
+match_split = 0
+for i in predict_split:
+	if i == expect_split[i]:
+		match_split += 1
+
+score_split = round((float(match_split) / len(test)) * 100, 1)
+print("Number of Matches: " + str(match_split))
+print("Score: " + str(score_split))
+print("")
